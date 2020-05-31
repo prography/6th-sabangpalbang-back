0204da88b17a5fcdcd4b2b589fe7aedf27b9ae78
KimGenius <geniusk1047@naver.com>
2020-05-31 22:53:31 +0900

    작업 전 코드 정리

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,8 @@
 import 'reflect-metadata'
 import { createExpressServer } from 'routing-controllers'
 import { createConnection } from 'typeorm'
-// import { User } from './entity/User'
-// TODO: 배포할땐 ormconfig.json 에서 synchronize false를 해야함미마
 const baseDir = __dirname
 createConnection().then(async (connection) => {
-  // const user = new User()
-  // user.firstName = 'Timber'
-  // user.lastName = 'Saw'
-  // user.age = 25
-  // await connection.manager.save(user)
-  // const users = await connection.manager.find(User)
   const app = createExpressServer({
     cors: true,
     controllers: [baseDir + '/controllers/*.ts'],
@@ -21,6 +13,7 @@ createConnection().then(async (connection) => {
   app.get('/ping', (req, res) => {
     return res.send('pong')
   })
+
   app.listen(3000, () => {
     console.log('서 버 러 닝')
   })
