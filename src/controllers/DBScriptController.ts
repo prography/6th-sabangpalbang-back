0371e5bb988ab40d8ae17abbb0793f4c56c29af4
KimGenius <geniusk1047@naver.com>
2020-06-01 23:59:22 +0900

    lint에 맞게 수정 및 예지 누나 리뷰 중 console.log 제거 작업

--- src/controllers/DBScriptController.ts
+++ src/controllers/DBScriptController.ts
@@ -1,29 +1,29 @@
 import {
   JsonController,
   Get,
-} from 'routing-controllers';
+} from 'routing-controllers'
 
 const csvManager = require('../../module/csvManager')
-import { Cocktail } from "../entity/Cocktail";
-import { Tag } from "../entity/Tag";
-import { CocktailHasTag } from '../entity/CocktailHasTag';
-import { Flavor } from '../entity/Flavor';
-import { CocktailHasFlavor } from '../entity/CocktailHasFlavor';
+import { Cocktail } from '../entity/Cocktail'
+import { Tag } from '../entity/Tag'
+import { CocktailHasTag } from '../entity/CocktailHasTag'
+import { Flavor } from '../entity/Flavor'
+import { CocktailHasFlavor } from '../entity/CocktailHasFlavor'
 
 
 @JsonController('/final/dbscripts')
 export class DBScriptController {
-
   @Get('/cocktails')
   public async cocktails() {
-
     try {
       const cocktailArr = await csvManager.read('cocktailData.csv')
 
-      cocktailArr.forEach(async element => {
-        let cocktail = new Cocktail()
-        let cocktailName = await Cocktail.find({ where: { name: element.name } });
-        if (!cocktailName.length) { //DB에 칵테일 중복값 없으면
+      for (const element of cocktailArr) {
+        const cocktail = new Cocktail()
+        const cocktailName = await Cocktail.find({
+          where: { name: element.name },
+        })
+        if (!cocktailName.length) { // DB에 칵테일 중복값 없으면
           cocktail.img_url = element.img_url
           cocktail.name = element.name
           cocktail.ingredients = element.ingredients
@@ -37,185 +37,164 @@ export class DBScriptController {
           }
 
           switch (element.base) {
-            case "없음":
-              cocktail.base_idx = 1;
-              break;
-            case "데킬라":
-              cocktail.base_idx = 2;
-              break;
-            case "럼":
-              cocktail.base_idx = 3;
-              break;
-            case "진":
-              cocktail.base_idx = 4;
-              break;
-            case "리큐어":
-              cocktail.base_idx = 5;
-              break;
-            case "보드카":
-              cocktail.base_idx = 6;
-              break;
-            case "브랜디":
-              cocktail.base_idx = 7;
-              break;
-            default: //기타
-              cocktail.base_idx = 8;
-              break;
+            case '없음':
+              cocktail.base_idx = 1
+              break
+            case '데킬라':
+              cocktail.base_idx = 2
+              break
+            case '럼':
+              cocktail.base_idx = 3
+              break
+            case '진':
+              cocktail.base_idx = 4
+              break
+            case '리큐어':
+              cocktail.base_idx = 5
+              break
+            case '보드카':
+              cocktail.base_idx = 6
+              break
+            case '브랜디':
+              cocktail.base_idx = 7
+              break
+            default: // 기타
+              cocktail.base_idx = 8
+              break
           }
-          console.log(cocktail);
-          Cocktail.save(cocktail)
+          await Cocktail.save(cocktail)
         }
-      })
+      }
     } catch (err) {
       console.log(err)
     }
-    return "cocktail T DBScript complete";
+    return 'cocktail T DBScript complete'
   }
 
 
-  //Tag T
+  // Tag T
   @Get('/tags')
   public async tags() {
-
     try {
       const cocktailArr = await csvManager.read('cocktailData.csv')
-      var tagData = []
-      cocktailArr.forEach(async element => {
-        var tagArr = element.tag.split(', ');
-        for (var i = 0; i < tagArr.length; i++) {
+      let tagData = []
+      for (const element of cocktailArr) {
+        const tagArr = element.tag.split(', ')
+        for (let i = 0; i < tagArr.length; i++) {
           tagData.push(tagArr[i])
         }
         tagData.sort()
-        tagData = Array.from(new Set(tagData));
-      })
-      console.log(tagData)
+        tagData = Array.from(new Set(tagData))
+      }
 
-      tagData.forEach(async element => {
+      for (const element of tagData) {
         if (element.length) {
-          console.log("element : ", element)
-          let dd = await Tag.find({ where: { name: element } });
+          const dd = await Tag.find({ where: { name: element } })
           if (dd.length == 0) {
-            console.log("await Tag.save(tag);");
-            let tag = new Tag()
+            const tag = new Tag()
             tag.name = element
-            Tag.save(tag);
+            await Tag.save(tag)
           }
         }
-      })
+      }
     } catch (err) {
       console.log(err)
     }
-    return "Tag T DBScript complete";
+    return 'Tag T DBScript complete'
   }
 
 
-
-  //Cocktail has tag
+  // Cocktail has tag
   @Get('/cocktailHasTag')
   public async cocktailHasTag() {
     try {
       const cocktailArr = await csvManager.read('cocktailData.csv')
-      cocktailArr.forEach(async element => {
-        console.log("--- 1 ---")
-        const cocktailIdx = (await Cocktail.find({ where: { name: element.name } }))[0].idx
-        console.log("cocktailIdx : " + cocktailIdx)
+      for (const element of cocktailArr) {
+        const cocktailIdx = (await Cocktail.find({
+          where: { name: element.name },
+        }))[0].idx
 
         if (cocktailIdx !== 0) {
-          console.log("--- 2 ---")
-          var tagArr = element.tag.split(', ')
-          tagArr.forEach(async element => {
-            let tagIdx = (await Tag.find({ where: { name: element } }))[0].idx
-            console.log("tagIdx : " + tagIdx)
-
+          const tagArr = element.tag.split(', ')
+          for (const element of tagArr) {
+            const tagIdx = (await Tag.find({ where: { name: element } }))[0].idx
             if (cocktailIdx !== 0 && tagIdx !== 0) {
-              console.log("--- 3 ---")
-              let cocktailHasTag = new CocktailHasTag()
+              const cocktailHasTag = new CocktailHasTag()
               cocktailHasTag.cocktail_idx = cocktailIdx
               cocktailHasTag.tag_idx = tagIdx
-              CocktailHasTag.save(cocktailHasTag)
-              console.log("cocktailHasTag save")
+              await CocktailHasTag.save(cocktailHasTag)
             }
-          })
+          }
         }
-
-      })
+      }
     } catch (err) {
       console.log(err)
     }
-    return "Tag T DBScript complete";
+    return 'Tag T DBScript complete'
   }
 
 
-  //Flavor T
+  // Flavor T
   @Get('/flavors')
   public async flavors() {
-
     try {
       const cocktailArr = await csvManager.read('cocktailData.csv')
-      var flavorData = []
-      cocktailArr.forEach(async element => {
-        var flavorArr = element.flavor.split(', ');
-        for (var i = 0; i < flavorArr.length; i++) {
+      let flavorData = []
+      for (const element of cocktailArr) {
+        const flavorArr = element.flavor.split(', ')
+        for (let i = 0; i < flavorArr.length; i++) {
           flavorData.push(flavorArr[i])
         }
         flavorData.sort()
-        flavorData = Array.from(new Set(flavorData));
-      })
-      console.log(flavorData)
+        flavorData = Array.from(new Set(flavorData))
+      }
 
-      flavorData.forEach(async element => {
+      for (const element of flavorData) {
         if (element.length) {
-          console.log("element : ", element)
-          let dd = await Flavor.find({ where: { name: element } });
+          const dd = await Flavor.find({ where: { name: element } })
           if (dd.length == 0) {
-            console.log("await Flavor.save(flavor);");
-            let flavor = new Flavor()
+            const flavor = new Flavor()
             flavor.name = element
-            Flavor.save(flavor);
+            await Flavor.save(flavor)
           }
         }
-      })
+      }
     } catch (err) {
       console.log(err)
     }
-    return "Flavor T DBScript complete";
+    return 'Flavor T DBScript complete'
   }
 
 
-
-
-  //Cocktail has flavor
+  // Cocktail has flavor
   @Get('/cocktailHasFlavor')
   public async cocktailHasFlavor() {
     try {
       const cocktailArr = await csvManager.read('cocktailData.csv')
-      cocktailArr.forEach(async element => {
-        console.log("--- 1 ---")
-        const cocktailIdx = (await Cocktail.find({ where: { name: element.name } }))[0].idx
-        console.log("cocktailIdx : " + cocktailIdx)
+      for (const element of cocktailArr) {
+        const cocktailIdx = (await Cocktail.find({
+          where: { name: element.name },
+        }))[0].idx
 
         if (cocktailIdx !== 0) {
-          console.log("--- 2 ---")
-          var flavorArr = element.flavor.split(', ')
-          flavorArr.forEach(async element => {
-            let flavorIdx = (await Flavor.find({ where: { name: element } }))[0].idx
-            console.log("flavorIdx : " + flavorIdx)
+          const flavorArr = element.flavor.split(', ')
+          for (const element of flavorArr) {
+            const flavorIdx = (await Flavor.find({
+              where: { name: element },
+            }))[0].idx
 
             if (cocktailIdx !== 0 && flavorIdx !== 0) {
-              console.log("--- 3 ---")
-              let cocktailHasFlavor = new CocktailHasFlavor()
+              const cocktailHasFlavor = new CocktailHasFlavor()
               cocktailHasFlavor.cocktail_idx = cocktailIdx
               cocktailHasFlavor.flavor_idx = flavorIdx
-              CocktailHasFlavor.save(cocktailHasFlavor)
-              console.log("cocktailHasFlavor save")
+              await CocktailHasFlavor.save(cocktailHasFlavor)
             }
-          })
+          }
         }
-
-      })
+      }
     } catch (err) {
       console.log(err)
     }
-    return "Flavor T DBScript complete";
+    return 'Flavor T DBScript complete'
   }
 }
