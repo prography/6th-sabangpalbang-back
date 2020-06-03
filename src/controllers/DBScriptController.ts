b7766b685687171b34807a24410782f53d4d940a
KimGenius <geniusk1047@naver.com>
2020-06-04 03:19:25 +0900

    베이스 이미지 링크 추가

--- src/controllers/DBScriptController.ts
+++ src/controllers/DBScriptController.ts
@@ -72,9 +72,20 @@ export class DBScriptController {
 
   async insertBase() {
     const baseNameList = ['없음', '데킬라', '럼', '진', '리큐어', '보드카', '브랜디', '기타']
+    const baseImgList = [
+      'https://user-images.githubusercontent.com/49062985/83237242-cd20c300-a1cf-11ea-9f36-a3b52345d9bc.jpg',
+      'https://user-images.githubusercontent.com/49062985/83227021-2ed83180-a1be-11ea-81f7-93c3eb99ba67.jpg',
+      'https://user-images.githubusercontent.com/49062985/82726176-39597d80-9d1d-11ea-8c90-090fe67b352e.png',
+      'https://user-images.githubusercontent.com/49062985/82726234-99e8ba80-9d1d-11ea-81f2-f13be5559a8b.png',
+      'https://user-images.githubusercontent.com/49062985/83226553-38ad6500-a1bd-11ea-886d-ace9026ed754.jpg',
+      'https://user-images.githubusercontent.com/49062985/83226647-6692a980-a1bd-11ea-9eec-a546a97c5229.jpg',
+      'https://user-images.githubusercontent.com/49062985/83226713-90e46700-a1bd-11ea-91b0-9735fad7e361.jpg',
+      'https://user-images.githubusercontent.com/49062985/83226826-ca1cd700-a1bd-11ea-859c-c1e2362111b0.jpg',
+    ]
+    let i = 0
     for (const baseName of baseNameList) {
-      // TODO: 이미지 링크, 도수, 설명 추가
-      await Base.saveData('https://naver.com', baseName, 40, '설명~')
+      // TODO: 도수, 설명 추가
+      await Base.saveData(baseImgList[i++], baseName, 40, '설명~')
     }
   }
 
