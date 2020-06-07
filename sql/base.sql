e52b4e729c32f3139a586c397fea66776747c3c4
KimGenius <geniusk1047@naver.com>
2020-06-08 02:34:04 +0900

    sql 폴더 삭제

--- sql/base.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO cocktender.base(img_url, name, abv) VALUES("https://user-images.githubusercontent.com/49062985/83237242-cd20c300-a1cf-11ea-9f36-a3b52345d9bc.jpg","없음",0);
-INSERT INTO cocktender.base(img_url, name, abv) VALUES("https://user-images.githubusercontent.com/49062985/83227021-2ed83180-a1be-11ea-81f7-93c3eb99ba67.jpg","데킬라",50);
-INSERT INTO cocktender.base(img_url, name, abv) VALUES("https://user-images.githubusercontent.com/49062985/82726176-39597d80-9d1d-11ea-8c90-090fe67b352e.png","럼",50);
-INSERT INTO cocktender.base(img_url, name, abv) VALUES("https://user-images.githubusercontent.com/49062985/82726234-99e8ba80-9d1d-11ea-81f2-f13be5559a8b.png","진",50);
-INSERT INTO cocktender.base(img_url, name, abv) VALUES("https://user-images.githubusercontent.com/49062985/83226553-38ad6500-a1bd-11ea-886d-ace9026ed754.jpg","리큐어",50);
-INSERT INTO cocktender.base(img_url, name, abv) VALUES("https://user-images.githubusercontent.com/49062985/83226647-6692a980-a1bd-11ea-9eec-a546a97c5229.jpg","보드카",50);
-INSERT INTO cocktender.base(img_url, name, abv) VALUES("https://user-images.githubusercontent.com/49062985/83226713-90e46700-a1bd-11ea-91b0-9735fad7e361.jpg","브랜디",50);
-INSERT INTO cocktender.base(img_url, name, abv) VALUES("https://user-images.githubusercontent.com/49062985/83226826-ca1cd700-a1bd-11ea-859c-c1e2362111b0.jpg","기타",50);
