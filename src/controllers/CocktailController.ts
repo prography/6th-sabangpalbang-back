2f0cf9f52cdf46b017a0822cf5ef0fed40b64c07
KimGenius <geniusk1047@naver.com>
2020-06-17 02:46:21 +0900

    이름 검색 작업

--- src/controllers/CocktailController.ts
+++ src/controllers/CocktailController.ts
@@ -1,11 +1,29 @@
-import { Get, JsonController, Param } from 'routing-controllers'
+import { Get, JsonController, Param, QueryParam } from 'routing-controllers'
 import { Cocktail } from '../entity/Cocktail'
+import { Between, getConnectionManager, LessThanOrEqual, Like, MoreThanOrEqual, Repository } from 'typeorm'
 
 @JsonController('/cocktails')
 export class CocktailController {
+  private cocktailRepository: Repository<Cocktail>;
+
+  constructor() {
+    this.cocktailRepository = getConnectionManager().get().getRepository(Cocktail)
+  }
+
   @Get('/')
-  async getCocktailList() {
-    const cocktails = await Cocktail.search()
+  async getCocktailList(
+    @QueryParam('tag') tag: string,
+    @QueryParam('base') base: string,
+    @QueryParam('name') name: string,
+    @QueryParam('abvMin') abvMin: number,
+    @QueryParam('abvMax') abvMax: number,
+  ) {
+    const where: any = {}
+    if (name) where.name = Like(`%${name}%`)
+    const cocktails = await this.cocktailRepository.find({
+      where,
+      relations: ['tags', 'flavors', 'base', 'abvClassification'],
+    })
     return { cocktails }
   }
 
