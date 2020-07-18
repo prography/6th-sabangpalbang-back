0210e32b8996ab85c47cde2f1905fbf421ecded0
KimGenius <geniusk1047@naver.com>
2020-07-18 22:37:40 +0900

    가입 flow 추가

--- src/controllers/OAuthController.ts
+++ src/controllers/OAuthController.ts
@@ -1,5 +1,6 @@
 import { Get, JsonController, QueryParam } from 'routing-controllers'
 import axios from 'axios'
+import { User } from '../entity/User'
 
 @JsonController('/oauth')
 export class UserController {
@@ -7,11 +8,35 @@ export class UserController {
   async redirectController(@QueryParam('code') code: string) {
     const codeData = await axios.get(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&code=${code}`)
     const accessToken = codeData.data.access_token
-    const userData = await axios.get(`https://kapi.kakao.com/v2/user/me`, {
+    const kakaoUserData = await axios.get(`https://kapi.kakao.com/v2/user/me`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     })
-    return userData.data
+    const existUserData = await User.findOne({ where: { kakaoID: kakaoUserData.data.id } })
+    if (!existUserData) {
+      // 데이터가 없으면 가입
+      const user = new User()
+      const {
+        id,
+        properties: {
+          nickname,
+          profile_image: profileImage,
+        },
+        kakao_account: {
+          email,
+        },
+      } = kakaoUserData.data
+      user.kakaoID = id
+      user.email = email
+      user.name = nickname
+      user.profileURL = profileImage
+      const saveData = await user.save()
+      console.log(saveData)
+      // TODO: token 발급
+    }
+    // 데이터가 있으면 로그인 진행
+    console.log(existUserData)
+    return kakaoUserData.data
   }
 }
