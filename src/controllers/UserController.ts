fd5afc6fea22b56bdc20a2c9c45dfefc53f52a86
KimGenius <geniusk1047@naver.com>
2020-07-19 04:01:51 +0900

    즐겨찾기 작업

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,10 @@
-import { BodyParam, HeaderParam, JsonController, Post, UseBefore } from 'routing-controllers'
+import { BodyParam, HeaderParam, JsonController, Patch, Post, UseBefore } from 'routing-controllers'
 import { UserAccessMiddleware } from '../middlewares/userAccessMiddleware'
 import { Review } from '../entity/Review'
 import { ConflictError } from '../http-errors/ConflictError'
 import { Cocktail } from '../entity/Cocktail'
 import { User } from '../entity/User'
+import { Like } from '../entity/Like'
 
 @UseBefore(UserAccessMiddleware)
 @JsonController('/users')
@@ -25,6 +26,29 @@ export class CocktailController {
     review.comment = comment
     review.user = user
     await review.save()
+    return {}
+  }
+
+  // 즐겨찾기 기능
+  // 이미 즐겨찾기가 되어있는거 한번 더 호출하면 즐겨찾기 해제
+  @Patch('/likes')
+  async like(
+    @HeaderParam('x-user-idx') userIdx: number,
+    @BodyParam('cocktailIdx') cocktailIdx: number,
+  ) {
+    const cocktail = await Cocktail.findOne(cocktailIdx)
+    const user = await User.findOne(userIdx)
+    const existLike = await Like.findOne({ where: { user, cocktail } })
+    if (existLike) {
+      // 즐겨찾기 삭제
+      await Like.delete({ user, cocktail })
+      return {}
+    }
+    // 즐겨찾기 추가
+    const like = new Like()
+    like.cocktail = cocktail
+    like.user = user
+    await like.save()
     return {}
   }
 }
